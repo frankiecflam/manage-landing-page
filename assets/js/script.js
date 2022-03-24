bd5a28b0b653eb68ad5731dd7c8f66c3a3df2ddd
Frankie Lam <frankie191193@gmail.com>
2022-03-24 18:35:29 +0000

    add form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ const testimonialsItems = document.querySelectorAll(".testimonials__item");
 const testiomonialsSliderController = document.querySelector(
   ".testimonials__slider-btnController"
 );
+const newsletterForm = document.querySelector(".secNav__newsletter-form");
+const newsletterEmailInput = document.querySelector(
+  ".secNav__newsletter-email"
+);
+const newsletterInputGroup = document.querySelector(
+  ".secNav__newsletter-inputGroup"
+);
 
 // Menu button
 navMenuBtn.onclick = () => {
@@ -41,10 +48,14 @@ function slideTo(slide) {
     item.style.transform = `translateX(${(i - slide) * 100}vw)`;
   });
 
-  const testimonialsSliderBtns = document.querySelectorAll(".testimonials__slider-btn");
+  const testimonialsSliderBtns = document.querySelectorAll(
+    ".testimonials__slider-btn"
+  );
   testimonialsSliderBtns.forEach((item, i) => {
-      +slide === i ? item.classList.add("active"): item.classList.remove("active");
-  })
+    +slide === i
+      ? item.classList.add("active")
+      : item.classList.remove("active");
+  });
 
   // Update current slidePointer
   currentSlide = +slide;
@@ -59,13 +70,74 @@ testiomonialsSliderController.onclick = function (e) {
 
 // Automatica Sliding after nth sec
 setInterval(() => {
-    // check if nextslide is out of range
-    if (currentSlide === maxSlide - 1) {
-        slideTo(minSlide);
-        currentSlide = minSlide;
-    }
-    else {
-        const nextslide = currentSlide + 1;
-        slideTo(nextslide);
-    }
+  // check if nextslide is out of range
+  if (currentSlide === maxSlide - 1) {
+    slideTo(minSlide);
+    currentSlide = minSlide;
+  } else {
+    const nextslide = currentSlide + 1;
+    slideTo(nextslide);
+  }
 }, 5000);
+
+// Clear validity
+function clearInputValidity(input) {
+  input.classList.remove("valid");
+  input.classList.remove("invalid");
+}
+
+function clearFormValidity(form) {
+  form.classList.remove("was-validated-success");
+  form.classList.remove("was-validated-unsuccess");
+}
+
+function checkInputValidity(input) {
+  const inputGroup = input.closest(".secNav__newsletter-inputGroup");
+  input.checkValidity()
+    ? inputGroup.classList.add("valid")
+    : inputGroup.classList.add("invalid");
+}
+
+function checkFormValidity(form) {
+  form.checkValidity()
+    ? form.classList.add("was-validated-success")
+    : form.classList.add("was-validated-unsuccess");
+}
+
+// Form validation
+newsletterForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Check form validity
+  checkFormValidity(newsletterForm);
+
+  // Clear input validity
+  clearInputValidity(newsletterInputGroup);
+
+  newsletterEmailInput.value = "";
+
+  // Dismiss form feedback after n seconds
+  setTimeout(() => {
+    clearFormValidity(newsletterForm);
+  }, 8000);
+
+});
+
+// Clear any previous form validity if there is input coming in
+newsletterEmailInput.addEventListener("focus", function () {
+  clearFormValidity(newsletterForm);
+});
+
+// Inline form validation when the input is off focus
+newsletterEmailInput.addEventListener("blur", function () {
+  clearInputValidity(newsletterInputGroup);
+
+  // No check necessary if input is left blank
+  if (!newsletterEmailInput.value.length > 0) return;
+
+  // show feedback depending on the input's validity
+  clearInputValidity(newsletterInputGroup);
+
+  // Check input validity
+  checkInputValidity(newsletterEmailInput);
+});
